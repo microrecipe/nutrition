79417af283c3b934305d02b4d22d9d0759e00a9e
Eris Ristemena <e.ristemena@gmail.com>
2022-11-18 09:42:54 +0700

    fix: listening IP

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const logger = new Logger('nutrition');
+  const logger = new Logger('Bootstrap');
 
   const app = await NestFactory.create(AppModule);
 
@@ -14,18 +14,20 @@ async function bootstrap() {
     options: {
       package: 'nutritions',
       protoPath: join(__dirname, '../src/nutritions.proto'),
-      url: `${process.env.NUTRITION_HOST}:${process.env.NUTRITION_GRPC_PORT}`,
+      url: `0.0.0.0:${process.env.NUTRITION_GRPC_PORT}`,
     },
   });
 
   await app.startAllMicroservices();
 
-  logger.verbose(`nutrition microservice is listening...`);
+  logger.log(
+    `gRPC service running on port: ${process.env.NUTRITION_GRPC_PORT}`,
+  );
 
   await app.listen(process.env.NUTRITION_REST_PORT);
 
-  logger.verbose(
-    `nutrition service running on port: ${process.env.NUTRITION_REST_PORT}`,
+  logger.log(
+    `HTTP service running on port: ${process.env.NUTRITION_REST_PORT}`,
   );
 }
 bootstrap();
